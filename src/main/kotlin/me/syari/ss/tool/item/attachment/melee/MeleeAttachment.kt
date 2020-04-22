f3363be4270afa6eedcf29b92322967ed8d76293
sya-ri <sya79lua@gmail.com>
2020-04-23 00:59:19 +0900

    MeleeAttachment::damageにattackerを追加しました

--- src/main/kotlin/me/syari/ss/tool/item/attachment/melee/MeleeAttachment.kt
+++ src/main/kotlin/me/syari/ss/tool/item/attachment/melee/MeleeAttachment.kt
@@ -3,12 +3,13 @@ package me.syari.ss.tool.item.attachment.melee
 import me.syari.ss.core.config.CustomConfig
 import me.syari.ss.tool.item.attachment.AttachmentLoader.getWearOut
 import org.bukkit.entity.LivingEntity
+import org.bukkit.entity.Player
 
 class MeleeAttachment(
     val wearOut: Int
 ) {
-    fun damage(entity: LivingEntity) {
-
+    fun damage(attacker: Player, victim: LivingEntity) {
+        victim.damage(0.0, attacker)
     }
 
     companion object {
